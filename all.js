3b72361ca71f79f94358ad17187b27ba44738dd2
Coco <coco35532442@gmail.com>
2022-04-21 16:32:32 +0800

    fix image upload

--- all.js
+++ all.js
@@ -1,3 +1,7 @@
+Quill.register("modules/imageUploader", ImageUploader);
+
+let imgUrl = 'https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
+
 //add the toolbar options
 const toolbarOptions= [
     ['bold', 'italic'],
@@ -7,6 +11,7 @@ const toolbarOptions= [
 
     // ['bold', 'italic', 'underline', 'strike'], // 粗體、斜體、底線和刪節線
     // ['blockquote', 'code-block'], // 區塊、程式區塊
+    // ['link', 'image', 'video'], // 連結、圖片、影片
     // [{ 'header': 1 }, { 'header': 2 }], // 標題1、標題2
     // [{ 'list': 'ordered'}, { 'list': 'bullet' }], // 清單
     // [{ 'script': 'sub'}, { 'script': 'super' }], // 上標、下標
@@ -26,24 +31,33 @@ var quill = new Quill('#editor', {
     modules: {
         toolbar: {
             container: toolbarOptions,
-            handlers: {
-                // handlers object will be merged with default handlers object
-                image: imageHandler,
-            }
+            // handlers: {
+            //     // handlers object will be merged with default handlers object
+            //     image: addImageURL,
+            // }
         },
         imageResize: {
             displaySize: true
         },
+        imageUploader: {
+            upload: file => {
+              return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  resolve(imgUrl);
+                }, 3500);
+              });
+            }
+          }
     },
     placeholder: '寫下產品描述',
 });
 
 
-function imageHandler() {
-    var range = this.quill.getSelection();
-    var value = prompt('please copy paste the image url here.');
-    if(value){
-        this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
-    }
-}
+// function addImageURL() {
+//     var range = this.quill.getSelection();
+//     var value = prompt('please copy paste the image url here.');
+//     if(value){
+//         this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+//     }
+// }
 
